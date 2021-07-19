0c4a6b3a188f5610b4ce81fae6cb1e281709c40c
EddyBautista <bautista.j.eddy@gmail.com>
2021-07-19 05:50:23 -0500

    Monday Coding challenge complete

--- Program.cs
+++ Program.cs
@@ -1,31 +1,55 @@
-﻿namespace Leetcode2021_July_CSharp
+﻿using System;
+using System.Linq;
+
+namespace Leetcode2021_July_CSharp
 {
     internal class Program
     {
-        // Day Two - Find K Closest Elements
+        // Complete the solution so that the function will break up camel casing,
+        // using a space between words.
 
-        //Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array.The result should also be sorted in ascending order.
+        // Example
+        // "camelCasing"  =>  "camel Casing"
+        // "identifier"   =>  "identifier"
+        // ""             =>  ""
 
-        //An integer a is closer to x than an integer b if:
-
-        //|a - x| < |b - x|, or
-        //|a - x| == |b - x| and a < b
-
-        //Example 1:
-
-        //Input: arr = [1, 2, 3, 4, 5], k = 4, x = 3
-        //Output: [1,2,3,4]
-        //Example 2:
-
-        //Input: arr = [1, 2, 3, 4, 5], k = 4, x = -1
-        //Output: [1,2,3,4]
-        public IList<int> FindClosestElements(int[] arr, int k, int x)
+        public static string BreakCamelCase(string str)
         {
+            // using counter to keep track of index 
+            int counter = 0;
 
+            // check to see if the string is empty or null
+            if(string.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+            // convert string to a char array 
+            char[] charArray = str.ToCharArray();
+            foreach (var item in charArray)
+            {
+                if (char.IsLower(item))
+                {
+                    counter++; // move on to the next index
+                }
+                if (char.IsUpper(item))
+                {
+                    str = str.Insert(counter , " " ); // when upper we want to insert a empty space at the last index before the upper case letter
+                    counter = counter + 2; // add the extra space and upper case index place
+                }
+            }
+            return str;
+        }
+
+        // using linq from solutions 
+        public static string BreakCamelCaseLinq(string str)
+        {
+            return string.Concat(str.Select(c => Char.IsUpper(c) ? " " + c : c.ToString()));
         }
 
         private static void Main(string[] args)
         {
+            BreakCamelCase("camelCasingTest");
+           
         }
     }
 }
